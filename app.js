82136179417e18c0ca3264562bfb573ddb216ea1
manojsharma16 <alexcham16@gmail.com>
2023-10-24 13:08:18 +0530

    app file origin fix2

--- app.js
+++ app.js
@@ -7,11 +7,15 @@ const appRoutes = require("./routers/approuters");
 const path = require("path");
 const app = express();
 
-app.use(cors({ origin: "https://nusasthesis.com" }));
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://nusasthesis.com");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-with, Content-Type, Accept"
+  );
   next();
 });
+app.use(cors({ origin: "https://nusasthesis.com" }));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
