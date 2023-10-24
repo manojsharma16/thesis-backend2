93de7fa1049538d3c5bd2be81d0daced928de636
manojsharma16 <alexcham16@gmail.com>
2023-10-24 23:55:06 +0530

    app file origin fix3

--- app.js
+++ app.js
@@ -7,15 +7,34 @@ const appRoutes = require("./routers/approuters");
 const path = require("path");
 const app = express();
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-with, Content-Type, Accept"
+//   );
+//   next();
+// });
+app.use(cors({ origin: "https://nusasthesis.com" }));
+app.use(function (req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  const allowedOrigins = [
+    "http://localhost:3000",
+    'http://nusasthesis.com"',
+    'https://nusasthesis.com"',
+  ];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-with, Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
   next();
 });
-app.use(cors({ origin: "https://nusasthesis.com" }));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
