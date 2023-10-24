57ae1f69e5b851f2ec44be0ef3e5c3d1006a1111
manojsharma16 <alexcham16@gmail.com>
2023-10-24 12:13:35 +0530

    app file origin fix

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const path = require("path");
 const app = express();
 
 app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
